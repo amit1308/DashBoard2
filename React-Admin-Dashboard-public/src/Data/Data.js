eeaf88c9fe3b3e7a3d24a489f30170fd93b5337b
Amit Kumar <amit.kumar.aaensatech@gmail.com>
2023-06-27 15:01:51 +0530

    To Deploy

--- React-Admin-Dashboard-public/src/Data/Data.js
+++ React-Admin-Dashboard-public/src/Data/Data.js
@@ -23,27 +23,27 @@ export const cardsData = [
  
   
   },
-  {
-    title: "Pressure",
-    color: {
-      backGround: "linear-gradient(180deg, #ed2137 0%, #FC929D 100%)",
-      boxShadow: "0px 10px 20px 0px #FDC0C7",
-    },
-    barValue: 80,
-    value: "14Pa",
-    png: UilMoneyWithdrawal,
+  // {
+  //   title: "Pressure",
+  //   color: {
+  //     backGround: "linear-gradient(180deg, #ed2137 0%, #FC929D 100%)",
+  //     boxShadow: "0px 10px 20px 0px #FDC0C7",
+  //   },
+  //   barValue: 80,
+  //   value: "14Pa",
+  //   png: UilMoneyWithdrawal,
  
-  },
-  {
-    title: "Humadity",
-    color: {
-      backGround:
-        "linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255 202 113) -46.42%)",
-      boxShadow: "0px 10px 20px 0px #F9D59B",
-    },
-    barValue: 60,
-    value: "20%",
-    png: UilClipboardAlt,
+  // },
+  // {
+  //   title: "Humadity",
+  //   color: {
+  //     backGround:
+  //       "linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255 202 113) -46.42%)",
+  //     boxShadow: "0px 10px 20px 0px #F9D59B",
+  //   },
+  //   barValue: 60,
+  //   value: "20%",
+  //   png: UilClipboardAlt,
  
-  },
+  // },
 ];
